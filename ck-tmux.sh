db41734e14a1179adad7bedeedcee582d8001b46
Joseph Rex <joerexko@gmail.com>
2021-05-20 09:04:43 -0500

    Add ck-tmux.sh

---
+++ ck-tmux.sh
@@ -0,0 +1,11 @@
+tmux new-session \; \
+  send-keys 'nvm use 14 && j functions && npm start' C-m \; \
+  split-window -v -p 50 \; \
+  send-keys 'nvm use 14 && j convertkit && ckdev' C-m \; \
+  split-window -v -p 50 \; \
+  send-keys 'j filekit && rails s' C-m \; \
+  split-window -h -p 50 \; \
+  send-keys 'j convertkit && ./bin/docker-dependencies' C-m \; \
+  select-pane -t 0 \; \
+  split-window -v -p 50 \; \
+  send-keys 'j convertkit && rails s' C-m \; \
