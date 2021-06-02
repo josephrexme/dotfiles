73f1bd10decc74fa55ac6aecfc56476c00ed14cb
Joseph Rex <joerexko@gmail.com>
2021-06-02 17:45:15 -0500

    Change from nvm to n

--- .zshrc
+++ .zshrc
@@ -88,13 +88,11 @@ base64decode() {
   echo "$1" | base64 --decode
 }
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 # Autojump
 [ -f $(brew --prefix)/etc/profile.d/autojump.sh ] && . $(brew --prefix)/etc/profile.d/autojump.sh
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 export FLASK_APP=blackjack
 export FLASK_ENV=development
+
+export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
--- ck-tmux.sh
+++ ck-tmux.sh
@@ -1,11 +1,11 @@
 tmux new-session \; \
-  send-keys 'nvm use 14 && j functions && npm start' C-m \; \
+  send-keys 'j functions && npm start' C-m \; \
   split-window -v -p 50 \; \
-  send-keys 'nvm use 14 && j convertkit && ckdev' C-m \; \
+  send-keys 'j convertkit && ckdev' C-m \; \
   split-window -v -p 50 \; \
   send-keys 'j filekit && rails s' C-m \; \
   split-window -h -p 50 \; \
   send-keys 'j convertkit && ./bin/docker-dependencies' C-m \; \
   select-pane -t 0 \; \
   split-window -v -p 50 \; \
-  send-keys '(sleep 40 && j convertkit && rails s)' C-m \; \
+  send-keys '(sleep 30 && j convertkit && rails s)' C-m \; \
