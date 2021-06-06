91edf532540b7cf6686cb9366816218d858d007a
Joseph Rex <joerexko@gmail.com>
2021-06-06 11:36:57 -0500

    Switch from autojump commands to path cd

--- ck-tmux.sh
+++ ck-tmux.sh
@@ -1,5 +1,5 @@
 if [ "$TERM_PROGRAM" = tmux ]; then
-  echo "exiting ck tmux session"
+  echo "Exiting ck tmux session"
   # Stop rails servers (ck and fk) and put down docker
   cat $CK_PATH/tmp/pids/server.pid | xargs -n1 -I pid kill -9 pid
   cat $FIELKIT_PATH/tmp/pids/server.pid | xargs -n1 -I pid kill -9 pid
@@ -7,14 +7,14 @@ if [ "$TERM_PROGRAM" = tmux ]; then
   tmux kill-session
 else
   tmux new-session \; \
-    send-keys 'j functions && npm start' C-m \; \
+    send-keys 'cd $CK_PATH/packages/functions && npm start' C-m \; \
     split-window -v -p 50 \; \
-    send-keys 'j convertkit && ckdev' C-m \; \
+    send-keys 'cd $CK_PATH && ckdev' C-m \; \
     split-window -v -p 50 \; \
-    send-keys '(sleep 30 && j filekit && rails s)' C-m \; \
+    send-keys '(sleep 30 && cd $FILEKIT_PATH && rails s)' C-m \; \
     split-window -h -p 50 \; \
     send-keys 'docker-compose --file $CK_PATH/dev/docker-compose-dependencies.yml up' C-m \; \
     select-pane -t 0 \; \
     split-window -v -p 50 \; \
-    send-keys '(sleep 30 && j convertkit && rails s)' C-m \;
+    send-keys '(sleep 30 && cd $CK_PATH && rails s)' C-m \;
 fi
