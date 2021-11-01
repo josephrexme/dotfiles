55b359a833677b05d2f4a0e4a9ae473ee854af76
Joseph Rex <joerexko@gmail.com>
2021-11-01 18:35:28 -0500

    Redo setup

--- setup.sh
+++ setup.sh
@@ -6,24 +6,21 @@ echo "Begin setting up Joseph's environment"
 apt-get update
 
 ## Prevent interactive questions from apt/dpkg
-DEBIAN_FRONTEND=noninteractive apt-get install -y \
+# `echo |` sends a return to STDIN for prompts
+echo | DEBIAN_FRONTEND=noninteractive apt-get install -y \
+  zsh \
   vim \
   git \
+  tmux \
   htop \
   curl
 
 # Install homebrew because it's easier to control than apt
-# The echo | is to send a single return via STDIN when installation
-# prompts
 echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /root/.profile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 brew install gcc
-
-# Install zsh with brew because apt will prompt about config
-# and it freezes setup
-brew install zsh
 
 # My brews needed by my zshconfig
 brew install hub
@@ -34,11 +31,11 @@ git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$H
 
 # Change to zsh
 # chsh -s "$(which zsh)" <- Getting PAM auth failure on this
-zsh
 
 
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+echo "Done setting up Joseph's environment"
 
