92e2ec810e560f47bcda42009a1fbed612dc36f3
Joseph Rex <joerexko@gmail.com>
2021-11-01 16:30:15 -0500

    Install zsh with homebrew instead of apt in setup.sh

--- setup.sh
+++ setup.sh
@@ -5,18 +5,10 @@ apt-get update
 
 ## Prevent interactive questions from apt/dpkg
 DEBIAN_FRONTEND=noninteractive apt-get install -y \
-  zsh \
   vim \
   git \
   htop \
   curl
-
-# Install prezto
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
-# Change to zsh
-chsh -s /bin/zsh
-zsh
 
 # Install homebrew because zsh configs use a lot of brew --prefix
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -25,9 +17,20 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 brew install gcc
 
+# Install zsh with brew because apt will prompt about config
+# and it freezes setup
+brew install zsh
+
 # My brews needed by my zshconfig
 brew install hub
 brew install lolcat
+
+# Install prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+# Change to zsh
+chsh -s /bin/zsh
+zsh
 
 
 # Install vim-plug
