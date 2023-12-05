5771323e622e5dd0632f5338bc87b71928938447
Joseph Rex <joseph.rex.t@gmail.com>
2023-12-05 09:49:10 -0600

    Use Mac M1 docker in ck-tmux

--- ck-tmux.sh
+++ ck-tmux.sh
@@ -6,7 +6,7 @@ if [ "$TERM_PROGRAM" = tmux ]; then
   # Stop functions JS server
   kill $(lsof -wni tcp:3031 | awk 'NR == 2 {print $2}')
   # Put down docker
-  docker-compose --file $CK_PATH/dev/docker-compose-dependencies.yml down
+  docker-compose --file $CK_PATH/dev/docker-compose-dependencies-m1.yml down
   tmux kill-session
 else
   tmux new-session \; \
@@ -16,7 +16,7 @@ else
     split-window -v -p 50 \; \
     send-keys '(sleep 30 && cd $FILEKIT_PATH && rails s)' C-m \; \
     split-window -h -p 50 \; \
-    send-keys 'docker-compose --file $CK_PATH/dev/docker-compose-dependencies.yml up' C-m \; \
+    send-keys 'docker-compose --file $CK_PATH/dev/docker-compose-dependencies-m1.yml up' C-m \; \
     select-pane -t 0 \; \
     split-window -v -p 50 \; \
     send-keys '(sleep 30 && cd $CK_PATH && rails s)' C-m \;
